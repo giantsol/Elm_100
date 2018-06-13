b046ae14c55721d79551ee42559030c1980b31bf
kevin-ee <kevin.ee@kakaocorp.com>
2018-06-13 15:51:11 +0900

    튜토리얼 프로젝트 진행 - Players list view 구현

--- elm-tutorial2/TutorialProject/src/Models.elm
+++ elm-tutorial2/TutorialProject/src/Models.elm
@@ -1,4 +1,23 @@
 module Models exposing (..)
 
 type alias Model =
+    {
+    players: List Player
+    }
+
+type alias Player =
+    {
+    id: PlayerId
+    , name: String
+    , level: Int
+    }
+
+type alias PlayerId =
     String
+
+initialModel: Model
+initialModel =
+    Model [
+        Player "1" "Sam" 1
+        , Player "2" "HeyMan" 1
+    ]
